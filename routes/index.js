c28c46b483bdfd7b2db9db968b4d72fa38b16ef0
Nick Watson <nick.watson@snyk.io>
2025-06-12 16:42:56 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,9 @@ var fs = require('fs');
 // prototype-pollution
 var _ = require('lodash');
 
+var token = 'SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9';
+console.log('token: ' + token);
+
 exports.index = function (req, res, next) {
   Todo.
     find({}).
