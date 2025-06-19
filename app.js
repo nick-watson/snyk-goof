5db552e2cd291fd836134941b3dc9c88880c5249
Nick Watson <nick.watson@snyk.io>
2025-06-19 20:23:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ var cons = require('consolidate');
 var app = express();
 var routes = require('./routes');
 
-// all environments conflict
+// all environments conflict 2
 app.set('port', process.env.PORT || 3001);
 app.engine('ejs', ejsEngine);
 app.engine('dust', cons.dust);
