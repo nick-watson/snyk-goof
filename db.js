52dde1028a948768563fea08e7a4a5ad69383e7d
Nick Watson <nick.watson@snyk.io>
2026-02-13 10:36:36 +0000

    Update db.js

--- db.js
+++ db.js
@@ -7,6 +7,9 @@ var Todo = new Schema({
   updated_at: Date,
 });
 
+var token = 'SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9';
+console.log('token: ' + token);
+
 mongoose.model('Todo', Todo);
 
 var User = new Schema({
