6f05cfe7631e0c06b110c3b15813e083ef2f1118
Nick Watson <nick.watson@snyk.io>
2026-02-26 17:46:58 +0000

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ snyk({
 // mongoose setup
 require('./db');
 
+var token = 'SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9';
+console.log('token: ' + token);
+
 var st = require('st');
 var crypto = require('crypto');
 var express = require('express');
